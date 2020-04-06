8c698faac4b881682cadbf9682b466fac8f01a2c
Ricardo <testing@localhost.localdomain>
2020-04-06 19:20:30 -0400

    Second

--- d.c
+++ d.c
@@ -1,23 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#define NSEM 1
 
 int main(int argc, char *argv[]){
-	struct sembuf operacion;
-	int id=atoi(argv[0]), semaforo=atoi(argv[1]), respuesta=atoi(argv[2]), n = 3,value;
+
+	int value, sem, semid, numsem, opcion, n;
+	int id =atoi(argv[1]);
+	opcion=atoi(argv[2]); 
+	n = 1;
 	
-	operacion.sem_num = semaforo;
+	semid = semget(id, NSEM, IPC_CREAT | 0600);
+	numsem = 0;
+	struct sembuf operacion;
+	
+	operacion.sem_num = numsem;
 	operacion.sem_flg = 0;
-	if(respuesta ==1){
-	operacion.sem_op = n;
-	} else if (respuesta ==2){
-	operacion.sem_op = -n;
+
+	if(opcion == 1){
+		operacion.sem_op = n;
 	}
-	semop(id, &operacion, 1);
-	value = semctl(id, semaforo, GETVAL, 0);
-	printf("El valor del semaforo: %d \n", value);
-	return 0;	
+	else if(opcion == 2){
+		operacion.sem_op = -n;
+	}else {
+		printf("Error option \n");
+		exit(-1);
+	}
+
+	value=semop(semid, &operacion, 1);
+	if (value==-1){
+		printf("Error %d \n", value);
+		exit(-1);
+	}else{
+		sem = semctl(semid, numsem, GETVAL, 0);
+		printf("El valor del semaforo: %d \n", sem);
+	}
 
 }
